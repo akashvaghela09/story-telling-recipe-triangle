3779d31850527d754e81cab8b98f2b633067cd6a
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-12-04 23:14:35 +0530

    Add Initial model

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-
+import TriangleImage from "./assets/triangle.svg.png";
 function App() {
   return (
     <div className="App">
-      REACT 18
+      <div className='triangle_container'>
+        <img src={TriangleImage} className='triangle' alt='triangle' />
+        <div className='button_container_top'>
+          <p className='state_button'>Title 1</p>
+        </div>
+        <div className='button_container_bottom'>
+          <p className='state_button button_1'>Title 2</p>
+          <p className='state_button button_1'>Title 3</p>
+        </div>
+      </div>
     </div>
+
   );
 }
 
