97b38fa7a6ccae7f7bcf378e02479d7398f2f2cd
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-12-04 23:29:47 +0530

    Add state change method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
 import TriangleImage from "./assets/triangle.svg.png";
 function App() {
+
+  const [primaryState, setPrimaryState] = useState("Story");
+  const [secondaryState1, setSecondaryState1] = useState("Emotion");
+  const [secondaryState2, setSecondaryState2] = useState("Rhythm");
+
+  const handleStateChange = (clickedState) => {
+    if (clickedState === secondaryState1) {
+      setSecondaryState1(primaryState);
+      setPrimaryState(clickedState);
+    } else if (clickedState === secondaryState2) {
+      setSecondaryState2(primaryState);
+      setPrimaryState(clickedState);
+    } else {
+      console.log("No state change");
+    }
+  }
+
   return (
     <div className="App">
       <div className='triangle_container'>
         <img src={TriangleImage} className='triangle' alt='triangle' />
         <div className='button_container_top'>
-          <p className='state_button'>Title 1</p>
+          <p onClick={() => handleStateChange(primaryState)} className='state_button'>{primaryState}</p>
         </div>
         <div className='button_container_bottom'>
-          <p className='state_button button_1'>Title 2</p>
-          <p className='state_button button_1'>Title 3</p>
+          <p onClick={() => handleStateChange(secondaryState1)} className='state_button button_1'>{secondaryState1}</p>
+          <p onClick={() => handleStateChange(secondaryState2)} className='state_button button_1'>{secondaryState2}</p>
         </div>
       </div>
     </div>
