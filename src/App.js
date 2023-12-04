938e2b83592e7cf2a7bef222700316c8246c0801
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-12-04 23:44:34 +0530

    Update triangle styles

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ function App() {
       <div className='triangle_container'>
         <img src={TriangleImage} className='triangle' alt='triangle' />
         <div className='button_container_top'>
-          <p onClick={() => handleStateChange(primaryState)} className='state_button'>{primaryState}</p>
+          <p onClick={() => handleStateChange(primaryState)} className='state_button primary_button button'>{primaryState}</p>
         </div>
         <div className='button_container_bottom'>
-          <p onClick={() => handleStateChange(secondaryState1)} className='state_button button_1'>{secondaryState1}</p>
-          <p onClick={() => handleStateChange(secondaryState2)} className='state_button button_1'>{secondaryState2}</p>
+          <p onClick={() => handleStateChange(secondaryState1)} className='state_button secondary_button button'>{secondaryState1}</p>
+          <p onClick={() => handleStateChange(secondaryState2)} className='state_button secondary_button button'>{secondaryState2}</p>
         </div>
       </div>
     </div>
